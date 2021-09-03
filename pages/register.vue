4460994c51beaa74ffb0526417866a75dd945090
yanuaraditia <yanuaraditia@outlook.com>
2021-09-04 01:31:42 +0700

    Status

--- pages/register.vue
+++ pages/register.vue
@@ -3,7 +3,7 @@
     <nav class="page-header py-3">
       <div class="container">
         <img src="https://kiriminaja.com/assets/atrust/svg-logo-primary.svg" class="mb-4" height="40px" alt="">
-        <h3 class="mb-3 mt-5 qsand">Masuk ke KiriminAja</h3>
+        <h3 class="mb-3 mt-5 qsand">Gabung ke KiriminAja</h3>
         <form action="">
           <div class="form-group">
             <label for="email" class="font-weight-medium">E-mail</label>
@@ -20,13 +20,13 @@
             </div>
             <input type="password" name="password" id="password" placeholder="Kata sandi anda" class="form-control">
           </div>
-          <button class="btn btn-primary btn-block">Masuk</button>
+          <button class="btn btn-primary btn-block">Daftar</button>
           <button class="btn btn-info btn-block mt-2">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8" /></svg>
-            <span class="px-3 d-inline-flex">Login dengan Google</span>
+            <span class="px-3 d-inline-flex">Gabung dengan Google</span>
           </button>
           <div class="mt-3 text-center">
-            Belum punya akun? <nuxt-link to="register">Daftar sekarang</nuxt-link>
+            Sudah punya akun? <nuxt-link to="login">Masuk sekarang</nuxt-link>
           </div>
         </form>
       </div>
